277817a75429090461ee6904e80eee7743ca3794
Eric Bruhns <bruhe002@gmail.com>
2022-04-21 18:55:24 -0700

    append() function

--- linklists/firstll.js
+++ linklists/firstll.js
@@ -10,9 +10,31 @@ class LinkedList {
 		this.tail = this.head;
 		this.length = 1;
 	}
+	
+	//Add to end of the list
+	append(val) {
+		this.tail.next = { //Set the tail.next to a new object
+			value: val,
+			next: null
+		}
+		this.tail = this.tail.next; // set tail to new object
+		this.length++;
+	}
 }
 
 
 const myLinkedList = new LinkedList(10);
 
 console.log(myLinkedList);
+
+myLinkedList.append(5);
+myLinkedList.append(16);
+console.log(myLinkedList);
+
+let current = myLinkedList.head;
+
+//Print all items in the list
+while(current !== null) {
+	console.log(current);
+	current = current.next;
+}

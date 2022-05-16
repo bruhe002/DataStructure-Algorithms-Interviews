4aef12cfc150ed4da0ca5e71872c75f296ce29fe
Eric Bruhns <bruhe002@gmail.com>
2022-05-16 16:54:46 -0700

    Adding nonrecursive binary search algorithm

--- sorting/binarySearching/binSearch.c
+++ sorting/binarySearching/binSearch.c
@@ -12,3 +12,22 @@ int binarySearch(const int arr[], int start, int end, int element)
 	if(arr[mid] < element) { return binarySearch(arr, mid + 1, end, element); }
 	if(arr[mid] > element) { return binarySearch(arr, start, mid - 1, element); }
 }
+
+int binarySearchNonRecursion(const int arr[], int start, int end, int element)
+{
+	while(end > start)
+	{
+		int mid = (end + start) / 2;
+
+		if(arr[mid] == element) { return mid; }
+		if(element > arr[mid]) {
+			start = mid + 1;
+		}
+		if(element < arr[mid])
+		{
+			end = mid - 1;
+		}
+	}
+
+	return -1;
+}
--- sorting/binarySearching/binSearch.h
+++ sorting/binarySearching/binSearch.h
@@ -2,5 +2,6 @@
 #define BINSEARCH_H
 
 int binarySearch(const int arr[], int, int, int);
+int binarySearchNonRecursion(const int arr[], int start, int end, int element);
 
 #endif
